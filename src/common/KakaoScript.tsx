4dc60f89a751d8fc3459e825bdacdb4248b3e746
junseo2323 <hunsol03@naver.com>
2025-10-18 16:06:20 +0900

    fix: kakaoSDK Init 수정

--- src/common/KakaoScript.tsx
+++ src/common/KakaoScript.tsx
@@ -1,11 +1,20 @@
 "use client";
 
 import Script from "next/script";
+import { useEffect } from "react";
 
 export default function KakaoScript() {
   const onLoad = () => {
-    window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_API_KEY);
+      if (typeof window !== "undefined" && window.Kakao) {
+        window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_API_KEY);
+      }
   };
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.Kakao) {
+      window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_API_KEY);
+    }
+  }, []);
+
 
   
   return (
