aafacbb78f1144712dabd5d5dd5fc069b551b6fc
junseo2323 <hunsol03@naver.com>
2025-10-18 16:34:02 +0900

    fix: Kakao SDK 사파리 오류

--- src/common/KakaoScript.tsx
+++ src/common/KakaoScript.tsx
@@ -13,10 +13,18 @@ export default function KakaoScript() {
   };
 
   return (
-    <Script
-      src="https://developers.kakao.com/sdk/js/kakao.js"
-      strategy="afterInteractive"
-      onLoad={onLoadKakao}
-    />
+      <Script
+        src="https://developers.kakao.com/sdk/js/kakao.js"
+        strategy="afterInteractive"
+        onLoad={() => {
+          console.log("Kakao script loaded, window.Kakao:", window.Kakao);
+          if (window.Kakao && !kakaoInitialized) {
+            window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_API_KEY);
+            kakaoInitialized = true;
+            console.log("Kakao SDK initialized");
+          }
+        }}
+        onError={() => console.log("❌ Kakao SDK 로드 실패")}
+      />
   );
 }
