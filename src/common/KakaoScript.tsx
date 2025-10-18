fae9af6e5f2352ba1a81cc5ead02d6f1a8f45034
junseo2323 <hunsol03@naver.com>
2025-10-18 16:45:51 +0900

    fix: Kakao SDK 사파리 오류

--- src/common/KakaoScript.tsx
+++ src/common/KakaoScript.tsx
@@ -1,39 +1,17 @@
 "use client";
 import Script from "next/script";
-import { useEffect } from "react";
-
-let kakaoInitialized = false;
 
 export default function KakaoScript() {
-      useEffect(() => {
-      if (typeof window === "undefined" || window.Kakao) return;
-
-      const script = document.createElement("script");
-      script.src = "https://developers.kakao.com/sdk/js/kakao.js";
-      script.onload = () => {
-        if (!window.Kakao.isInitialized()) {
-          window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_API_KEY);
-          console.log("✅ Kakao SDK initialized");
-        }
-      };
-      script.onerror = () => console.log("❌ Kakao SDK load failed");
-      document.head.appendChild(script);
-    }, []);
-
-
   return (
-      <Script
-        src="https://developers.kakao.com/sdk/js/kakao.js"
-        strategy="afterInteractive"
-        onLoad={() => {
-          console.log("Kakao script loaded, window.Kakao:", window.Kakao);
-          if (window.Kakao && !kakaoInitialized) {
-            window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_API_KEY);
-            kakaoInitialized = true;
-            console.log("Kakao SDK initialized");
-          }
-        }}
-        onError={() => console.log("❌ Kakao SDK 로드 실패")}
-      />
+    <Script
+      src="https://developers.kakao.com/sdk/js/kakao.js"
+      strategy="afterInteractive"
+      onLoad={() => {
+        if (window.Kakao && !window.Kakao.isInitialized()) {
+          window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_API_KEY);
+        }
+      }}
+      onError={() => console.log("❌ Kakao SDK 로드 실패")}
+    />
   );
 }
