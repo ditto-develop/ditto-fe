d8dd624cf9bde3ec9b9a8ebe4d159bbd3d3ccb80
junseo2323 <hunsol03@naver.com>
2025-12-04 11:23:30 +0530

    test: action test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div>
-      
+      <h1>TEST PAGE</h1>
+      <p>hello Ditto! 새로운 디토.</p>
     </div>
   );
 }
